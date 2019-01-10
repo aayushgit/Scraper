5372c803b8a97623d25c6a2fef88e98d2ceac15b
Aayush Sharma Giri <aayush@Aayushs-MacBook-Pro.local>
2019-01-10 07:51:41 +0545

    Gets Urls

--- handbscrape.py
+++ handbscrape.py
@@ -17,42 +17,47 @@ home_html = driver.page_source
 soup  = BeautifulSoup(home_html,'lxml')
 # Get all the categories and their root URLS
 categories=[]
-hier=[]
+all_products =[]
 category_container = soup.find('div', class_='category-cartridge-desktop') 
 # print(category_container)
 for links in  category_container.find_all('a',href=True):
     # Categories URLs
     categories.append(base_url+ links['href'])
 # print(categories)
-single_url = categories[1]
-driver.get(single_url)
-#HTML_Content
-single_html = driver.page_source
-# close connection
-page_soup = BeautifulSoup(single_html,'lxml')
-# print(page_soup)
-# Get the View All Link
-if page_soup.find('a',alt='View All',href=True):
-    all_link_soup = page_soup.find('a',alt='View All',href=True)
+for single_url in categories:
+    driver.get(single_url)
+    #HTML_Content
+    single_html = driver.page_source
+    # close connection
+    page_soup = BeautifulSoup(single_html,'lxml')
+    # print(page_soup)
+    # Get the View All Link
+    if page_soup.find('a',alt='View All',href=True):
+        all_link_soup = page_soup.find('a',alt='View All',href=True)
+        # Convert text into actual link
+        all_link = base_url+ all_link_soup['href']
+    elif page_soup.find('a',alt='Shop All',href=True):
+        all_link_soup = page_soup.find('a',alt='Shop All',href=True)
+    else:
+        print("No View All/ Shop All")
+        pass
     # Convert text into actual link
     all_link = base_url+ all_link_soup['href']
-elif page_soup.find('a',alt='Shop All',href=True):
-    all_link_soup = page_soup.find('a',alt='Shop All',href=True)
-else:
-    pass
-# Convert text into actual link
-all_link = base_url+ all_link_soup['href']
-driver.get(all_link)
-# HTML Content of Products Listing Page
-try:
-    element = WebDriverWait(driver,10).until(
-        EC.presence_of_all_elements_located((By.ID, "ajaxLoaded"))
-    )
-except:
-    print("Nope")
-    driver.quit()
+    driver.get(all_link)
+    # HTML Content of Products Listing Page
+    try:
+        element = WebDriverWait(driver,10).until(
+            EC.presence_of_all_elements_located((By.ID, "ajaxLoaded"))
+        )
+    except:
+        print("Nope")
+        driver.quit()
 
-product = driver.find_element_by_xpath('//*[@id="scroll-after-filter-apply"]/div[2]/section/div/ul').text
-print(product)
-# product_soup= BeautifulSoup(product,'lxml')
-# print(product_soup)
+    product_page = driver.find_element_by_xpath('//*[@id="scroll-after-filter-apply"]/div[2]/section/div/ul').get_attribute('innerHTML')
+    product_soup= BeautifulSoup(product_page,'lxml')
+    for products in product_soup.find_all('a',href= True):
+        all_products.append(base_url+products['href'])
+
+for a in all_products:
+    print(a)
+
